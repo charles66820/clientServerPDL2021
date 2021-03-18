1355dc08c4a03bfcd062f7d5418161c062d2297b
Charles Goedefroit <charles.goedefroit@etu.u-bordeaux.fr>
2021-03-18 15:59:37 +0100

    add frontend structure

---
+++ frontend/src/components/Images.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <!-- TODO: carousel -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Images",
+  data() {
+    return {
+      images: [],
+      errors: [],
+    };
+  },
+  methods: {},
+  mounted: function () {},
+};
+</script>
+
+<style scoped>
+/* TODO: carousel css */
+</style>
---
+++ frontend/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>This is an about page</h1>
+    <!-- TODO: about -->
+  </div>
+</template>
