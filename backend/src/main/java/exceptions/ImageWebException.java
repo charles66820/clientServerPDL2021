fa4135f53173281c9e9a771666957b6703ba4771
Charles Goedefroit <charles.goedefroit@etu.u-bordeaux.fr>
2021-04-12 00:25:29 +0200

    type factorisation

--- backend/src/main/java/exceptions/ImageWebException.java
+++ backend/src/main/java/exceptions/ImageWebException.java
@@ -5,8 +5,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.HttpStatus;
 
 public class ImageWebException extends Exception {
-    private ObjectMapper mapper = new ObjectMapper();
-    private String type;
+    public final ObjectMapper mapper = new ObjectMapper();
     private String message;
     public final HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
@@ -19,22 +18,14 @@ public class ImageWebException extends Exception {
         super();
     }
 
-    public ObjectMapper getMapper() {
-        return mapper;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
     /**
      * Create a node in JSON with all informations of the exception
      * @return node in JSON
      */
     public ObjectNode toJSON() {
         ObjectNode node = this.mapper.createObjectNode();
-        node.put("Type", this.type);
-        node.put("Message", this.message);
+        node.put("type", getClass().getSimpleName());
+        node.put("message", this.message);
 
         return node;
     }
