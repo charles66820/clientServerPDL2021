2ee1ecff9aabdcb382c34f0367f85f19f2563ecb
Amandine_Semeteys <amandine.semeteys@etu.u-bordeaux.fr>
2021-04-12 20:01:06 +0200

    dimenSion

--- backend/src/main/java/pdl/backend/ImageController.java
+++ backend/src/main/java/pdl/backend/ImageController.java
@@ -104,8 +104,8 @@ public class ImageController {
             long fileSize = (long) imageMetaData.get("size");
             long width = (long) imageMetaData.get("width");
             long height = (long) imageMetaData.get("height");
-            long dimention = (long) imageMetaData.get("dimension");
-            String size = String.format("%d*%d*%d", width, height, dimention);
+            long dimension = (long) imageMetaData.get("dimension");
+            String size = String.format("%d*%d*%d", width, height, dimension);
 
             Image image = new Image(file.getOriginalFilename(), filecontent, file.getContentType(), size, fileSize);
             imageDao.create(image);
