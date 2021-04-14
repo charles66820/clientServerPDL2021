2522471e7413ecd8d3251b602a42b30d4d15170e
Amandine_Semeteys <amandine.semeteys@etu.u-bordeaux.fr>
2021-04-14 20:57:19 +0200

    add about eng + fr

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>{{ t("components.about.title") }}</h1>
-    <!-- TODO: about -->
+    <p>{{ t("components.about.content1") }}</p>
+    <p>{{ t("components.about.content2") }}</p>
+    <p>{{ t("components.about.content3") }}</p>
+    <p>{{ t("components.about.content4") }}</p>
+    <p>{{ t("components.about.content5") }}</p>
+    <p>{{ t("components.about.content6") }}</p>
   </div>
 </template>
 
@@ -20,3 +25,17 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+h1 {
+  margin-left: 10px;
+}
+
+p {
+  margin-left: 10px;
+  margin-right: 50px;
+  font-size: large;
+  text-align: center;
+}
+
+</style>
