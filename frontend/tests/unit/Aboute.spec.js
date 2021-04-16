011add496b350b7122f881d834a3d24ce126673b
Charles Goedefroit <charles.goedefroit@etu.u-bordeaux.fr>
2021-04-16 03:09:53 +0200

    A very important test for life

---
+++ frontend/tests/unit/Aboute.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils'
+// import App from '@/App.vue'
+
+describe('The life is cool', () => {
+  it('renders ', () => {
+    //const wrapper = shallowMount(App)
+    //wrapper.text()
+    expect("true").toMatch("true")
+  })
+})
